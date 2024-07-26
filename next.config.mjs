31c0686452cace9329f966e36ce602713b182eb1
nkansah-wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2024-07-25 22:33:07 -0500

    chore: Update next.config.mjs and package.json

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images:{
+        remotePatterns: [
+            {
+              protocol: 'https',
+              hostname: 'storage.googleapis.com',
+              port: '',
+              pathname: '/proudcity/mebanenc/uploads/2021/03/**',
+            },
+          ],
+    }
+};
 
 export default nextConfig;
