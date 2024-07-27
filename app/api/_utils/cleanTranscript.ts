71d2dcbad0397b180275ef5a90075aa9624d2793
nkansah-wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2024-07-26 22:12:38 -0500

    refactor: Clean and decode text in cleanTranscript.ts

---
+++ app/api/_utils/cleanTranscript.ts
@@ -0,0 +1,18 @@
+import he from "he";
+
+export function cleanText(text: string) {
+    console.log("Cleaning Text");
+    console.log(text);
+  
+    // Add spaces around HTML entities
+    const spacedText = text.replace(/(&[a-zA-Z0-9#]+;)/g, ' $1 ');
+    console.log("Spaced Text");
+    console.log(spacedText);
+  
+    // Decode the HTML entities
+    const cleanedText = he.decode(spacedText);
+    console.log("Cleaned Text");
+    console.log(cleanedText);
+  
+    return cleanedText;
+  }
--- app/api/_utils/computeDuration.ts
+++ app/api/_utils/computeDuration.ts
@@ -1,33 +1,35 @@
-import { off } from "process";
+import { cleanText } from "./cleanTranscript";
 
 export type Transcript = {
-    text: string;
-    duration: number;
-    offset: number;
-    lang: string;
+  text: string;
+  duration: number;
+  offset: number;
+  lang: string;
+};
 
-}
+export const computeDuration = (transcripts: Transcript[]) => {
+  const sortedTranscripts = transcripts.sort((a, b) => a.offset - b.offset);
 
-export const computeDuration=(transcripts: Transcript[])=>{
+  const newTranscripts = sortedTranscripts.map((transcript, index) => {
+    const startTime = convertSecondsToMinutes(transcript.offset);
+    const endTime = convertSecondsToMinutes(
+      transcript.offset + transcript.duration
+    );
 
-        const newTranscripts = transcripts.map((transcript, index) => {
-          const startTime = convertSecondsToMinutes(transcript.offset);
-          const endTime = convertSecondsToMinutes(transcript.offset + transcript.duration);
-        
-            return{
-                text: transcript.text,
-                startTime,
-                endTime,
-                duration: transcript.duration,
-                offset: transcript.offset,
-            }
-        })
+    return {
+      text: cleanText(transcript.text),
+      startTime,
+      endTime,
+      duration: transcript.duration,
+      offset: transcript.offset,
+    };
+  });
 
-        return newTranscripts;
-}
+  return newTranscripts;
+};
 function convertSecondsToMinutes(seconds: number) {
-    let minutes = Math.floor(seconds / 60); // Get the whole number of minutes
-    let remainingSeconds = (seconds % 60).toFixed(2); // Get the remaining seconds
+  let minutes = Math.floor(seconds / 60); // Get the whole number of minutes
+  let remainingSeconds = (seconds % 60).toFixed(2); // Get the remaining seconds
 
-    return `${minutes}:${remainingSeconds}`;
+  return `${minutes}:${remainingSeconds}`;
 }
