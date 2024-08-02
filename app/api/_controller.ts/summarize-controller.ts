4d5243503d7075364cc88c963a1a776930054e7c
nkansah-wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2024-08-01 22:49:24 -0500

    feat: Add summarizeController API endpoint and utility function for AI text summarization

---
+++ app/api/_controller.ts/summarize-controller.ts
@@ -0,0 +1,23 @@
+import { getChunks } from "../_ai/getChunks";
+import { Transcript } from "../_utils/computeDuration";
+import { extracterTranscriptController } from "./extracter-transcript-controller";
+
+export const summarizeController = async (url: string) => {
+    try {
+
+        const transcript = await extracterTranscriptController(url);
+        if (!transcript || transcript instanceof Error) {
+            return new Error("Error in SummarizeController");
+        }
+
+        const transcriptScript = transcript.map((transcript: Transcript) => transcript.text).join(" ");
+
+        const chunks = await getChunks(transcriptScript);
+
+        return chunks;
+
+    } catch (error) {
+        console.error(error);
+        return new Error("Error in SummarizeController");
+    }
+}
